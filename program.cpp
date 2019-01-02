f1f4b4ff37dc646c871339d9e2a30f401525a333
Jelena <cvetakg@gmail.com>
2019-01-02 06:36:16 -0800

    first program

---
+++ program.cpp
@@ -0,0 +1,19 @@
+// Author: Jelena Kocic
+// Date: January 2019
+
+#include <iostream>
+#include <stdio.h>
+
+//#include <cv.h>
+//#include <highgui.h>
+
+//#include <unistd.h>
+
+//using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv )
+{
+    cout << "Hello, 2019!\n";
+    return 0;
+}
